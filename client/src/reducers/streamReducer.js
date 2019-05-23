d64070570ac1eafa573c69ad0a537b2a23dbe188
Brian Espinosa <bespinosa1931@gmail.com>
2019-05-23 17:42:34 -0500

    Deletes 'Merge list of records, fetch list of streams, render list

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -12,7 +12,7 @@ export default (state = {}, action) => {
 		case FETCH_STREAM:
 			return { ...state, [action.payload.id]: action.payload };
 		case FETCH_STREAMS:
-			return;
+			return { ...state, ..._.mapKeys(action.payload, "id") };
 		case CREATE_STREAM:
 			return { ...state, [action.payload.id]: action.payload };
 		case EDIT_STREAM:
