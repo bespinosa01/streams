d65e1f377a13606f15f231d79e12cf748d3ea6ff
Brian Espinosa <bespinosa1931@gmail.com>
2019-05-23 19:21:43 -0500

    Associating streams with users, & conditionaly showing buttons

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,15 +1,28 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { fetchStreams } from "../../actions";
 
 class StreamList extends React.Component {
 	componentDidMount() {
 		this.props.fetchStreams();
 	}
+	renderAdmin(stream) {
+		if (stream.userId === this.props.currentUserId) {
+			return (
+				<div className="right floated content">
+					<button className="ui button primary">Edit</button>
+					<button className="ui button negative">Delete</button>
+				</div>
+			);
+		}
+	}
 	renderList() {
 		return this.props.streams.map(stream => {
 			return (
 				<div className="item" key={stream.id}>
+					{this.renderAdmin(stream)}
+
 					<i className="large middle aligned icon camera" />
 					<div className="content">
 						{stream.title}
@@ -19,6 +32,7 @@ class StreamList extends React.Component {
 			);
 		});
 	}
+
 	render() {
 		return (
 			<div>
@@ -30,7 +44,11 @@ class StreamList extends React.Component {
 }
 
 const mapStateToProps = state => {
-	return { streams: Object.values(state.streams) };
+	return {
+		streams: Object.values(state.streams),
+		currentUserId: state.auth.userId,
+		isSignedIn: state.auth.isSignedIn
+	};
 };
 export default connect(
 	mapStateToProps,
