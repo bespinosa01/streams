4e5d12f2d475543bab76099e24cc0f5ac3af4c0a
Brian Espinosa <bespinosa1931@gmail.com>
2019-05-28 13:36:34 -0500

    conditionally showing stream details

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -18,22 +18,30 @@ class StreamDelete extends React.Component {
 		);
 	}
 
+	renderContent() {
+		if (!this.props.stream) {
+			return "Are you sure you want to delete this stream?";
+		}
+		return `Are you sure you want to delete ${
+			this.props.stream.title
+		} from Streams?`;
+	}
+
 	render() {
 		return (
-			<div>
-				StreamDelete
-				<Modal
-					title="Delete Stream"
-					content="Are you sure you want to delete this stream?"
-					actions={this.renderActions()}
-					onDismiss={() => history.push("/")}
-				/>
-			</div>
+			<Modal
+				title="Delete Stream"
+				content={this.renderContent()}
+				actions={this.renderActions()}
+				onDismiss={() => history.push("/")}
+			/>
 		);
 	}
 }
-
+const mapStateToProps = (state, ownProps) => {
+	return { stream: state.streams[ownProps.match.params.id] };
+};
 export default connect(
-	null,
+	mapStateToProps,
 	{ fetchStream }
 )(StreamDelete);
